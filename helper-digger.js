c24e6a3b18a4454858832be2f4564e59702fa330
Sotatek-ThanhLe2 <thanh.le2@sotatek.com>
2022-03-19 15:43:56 +0700

    demo v4

--- helper-digger.js
+++ helper-digger.js
@@ -8,21 +8,18 @@ window.web3gl.digger = {
 
 async function diggerMint(count) {
   try {
-    alert(count);
-    await diggerContract.methods.mint(2).call();
+    await diggerContract.methods.mint(count).call();
   } catch (error) {
     console.log(error, 'err');
   }
 }
 
 async function diggerGetClaimableTokens(address) {
-  alert(address);
   await diggerContract.methods.getClaimableTokens(address).call();
 }
 
 async function diggerGetProcessableTokens(address) {
   try {
-    alert(address);
     const rs = await diggerContract.methods
       .getProcessableTokens(address)
       .call();
--- helper-house.js
+++ helper-house.js
@@ -1,15 +1,28 @@
 const houseContract = new web3.eth.Contract(abiDigger, HOUSE_CONTRACT);
 
 window.web3gl.house = {
-  withdraw,
-  burn,
+  houseMint,
+  houseWithdraw,
+  houseBurn,
+  houseGetTokenDetailsByOwner,
 };
 
-async function withdraw() {
+async function houseMint(rarity) {
+  if (typeof rarity !== 'number') return;
+  await houseContract.methods.mint(rarity).call();
+}
+
+async function houseWithdraw() {
   await houseContract.methods.withdraw().call();
 }
 
-async function burn(ids) {
+async function houseBurn(ids) {
   if (!Array.isArray(ids) || !ids?.length) return;
   await houseContract.methods.burn(ids).call();
+}
+
+async function houseGetTokenDetailsByOwner(address) {
+  if (typeof address !== 'string') return;
+  const rs = await houseContract.methods.getTokenDetailsByOwner(address).call();
+  return rs;
 }
